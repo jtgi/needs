0130600dd2a37d36c60c34493312c166e88693b9
jtgi <j@jtgi.me>
2017-06-01 21:11:18 -0700

    so basic

--- needs.py
+++ needs.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+from twilio import twiml
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-	return "<h1>wow</h1>"
+@app.route("/", methods=['POST'])
+def add():
+    number = request.form['From']
+    message_body = request.form['Body']
+
+    if number and message_body:
+        res = twiml.Response()
+        res.message("We've alerted the sheriff. Thanks.")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
