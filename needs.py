92fc1ec428b5cad4ae7906a4f64f9743db6f320b
jtgi <j@jtgi.me>
2017-06-02 00:13:52 -0700

    update sheet

--- needs.py
+++ needs.py
@@ -1,19 +1,32 @@
+import gspread
+import fortunes
+import datetime
+
 from flask import Flask, request
+from random import randint
 from twilio.twiml.messaging_response import MessagingResponse
+from oauth2client.service_account import ServiceAccountCredentials
 
 app = Flask(__name__)
+
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+sheet = client.open("Revolver Needs").sheet1
 
 @app.route("/", methods=['POST'])
 def add():
     number = request.form['From']
     message_body = request.form['Body']
+    message = "Something went wrong. Try again?"
 
     if number and message_body:
-        res = MessagingResponse()
-        res.message("We've alerted the sheriff. Thanks.")
-        return str(res)
+        time = datetime.datetime.now().strftime("%Y-%m-%d")
+        sheet.insert_row([time, number, message_body], 2)
+        fort = fortunes.list[randint(0, len(fortunes.list))]
+        message = "The sheriff has been alerted.\nYour fortune: {}.".format(fort)
 
-    return "something bad happened"
+    return str(MessagingResponse().message(message))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
