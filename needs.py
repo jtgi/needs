08ae8a2e8843a1933f6fca2be79948d0caf087df
jtgi <j@jtgi.me>
2017-06-06 17:15:41 +0000

    in time range, vars

--- needs.py
+++ needs.py
@@ -1,7 +1,7 @@
 import gspread
 import fortunes
 import datetime
-import os
+import needscreds
 
 from flask import Flask, request
 from random import randint
@@ -11,17 +11,17 @@ from twilio.rest import Client
 
 app = Flask(__name__)
 
-ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
-AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
 sheriff = "+16047209499"
 #sheriff = "+12063932973"
-inbound = "+16046702633"
+innbound = "+16046702633"
+start = datetime.time(1, 0, 0)
+end = datetime.time(9, 0, 0)
 
 scope = ['https://spreadsheets.google.com/feeds']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 sheet = client.open("Revolver Needs").sheet1
-twilio_client =  Client(ACCOUNT_SID, AUTH_TOKEN)
+twilio_client =  Client(needscreds.TWILIO_ACCOUNT_SID, needscreds.TWILIO_AUTH_TOKEN)
 
 @app.route("/", methods=['POST'])
 def add():
@@ -34,10 +34,19 @@ def add():
         sheet.insert_row([time, number, message_body], 2)
         fort = fortunes.list[randint(0, len(fortunes.list))]
         message = "The sheriff has been alerted.\nYour fortune: {}.".format(fort)
-	sheriff_msg = "New task:\n\n{}\nFrom: {}\n\nneeds.revolvercoffee.ca".format(message_body, number)
-	twilio_client.messages.create(to=sheriff, from_=inbound, body=sheriff_msg)
+
+	if time_in_range(start, end, datetime.datetime.now().time()):
+		sheriff_msg = "New task:\n\n{}\nFrom: {}\n\nneeds.revolvercoffee.ca".format(message_body, number)
+		twilio_client.messages.create(to=sheriff, from_=inbound, body=sheriff_msg)
 
     return str(MessagingResponse().message(message))
+
+def time_in_range(start, end, x):
+    """Return true if x is in the range [start, end]"""
+    if start <= end:
+        return start <= x <= end
+    else:
+        return start <= x or x <= end
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
